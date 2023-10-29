7d8cc83693fc50ad3a8f698e79b63798daf1f0ce
Akshat Rawat <akshatrawat84@gmail.com>
2023-10-29 18:29:36 +0530

    Add clamp and mousechaptakaro function

--- PROJECT1 Portfolio/script.js
+++ PROJECT1 Portfolio/script.js
@@ -31,9 +31,46 @@ function firstPageAnim(){
 
 }
 
-function circleMouseFollower(){
+// jab mouse move ho to hum log skew kar paaye aur maximum skew define kar paaye , jab mouse move ho to chapta ki value badhe , aur jab mouse chalna band ho jaaye to chapta hata lo
+
+var timeout
+function circleChaptakaro(){
+
+    // Define default sacel value 
+    var xscale = 1 ;
+    var yscale = 1 ;
+
+    var xprev = 0 ;
+    var yprev = 0 ;
+
+
+    window.addEventListener("mousemove" , function(dets){
+
+        clearTimeout(timeout);
+
+        // Using clamp 
+        
+        xscale = gsap.utils.clamp(.8,1.2, dets.clientX - xprev);
+
+        yscale = gsap.utils.clamp(.8,1.2,dets.clientX - yprev);
+
+        xprev = dets.clientX ;
+        yprev = dets.clientY ;
+
+        circleMouseFollower(xscale , yscale);
+
+       timerout = setTimeout(function(){
+            document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px , ${dets.clientY}px) scale(${1}, ${1})`;
+
+        }, 100)
+    });
+}
+
+circleChaptakaro(); 
+
+function circleMouseFollower(xscale, yscale){
     window.addEventListener("mousemove",function(dets){
-        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px , ${dets.clientY}px)`;
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px , ${dets.clientY}px) scale(${xscale}, ${yscale})`;
     })
 }
 
