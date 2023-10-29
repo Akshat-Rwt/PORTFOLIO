f84a07a497720b48b5dcb164b745fe3ab518c1fd
Akshat Rawat <akshatrawat84@gmail.com>
2023-10-29 15:40:29 +0530

    Attach mouse Function which follow cursor

--- PROJECT1 Portfolio/script.js
+++ PROJECT1 Portfolio/script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove",function(dets){
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px , ${dets.clientY}px)`;
+    })
+}
+
+circleMouseFollower();
